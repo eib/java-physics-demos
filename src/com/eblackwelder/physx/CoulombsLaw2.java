e704fd96d7aedb28996d11367b9951123845518f
eib <eib@github>
2012-06-19 09:29:49 -0400

    Coulomb: Abstracting CoulombsLaw's collision-handling to an arbitrary number of particles.

--- src/com/eblackwelder/physx/CoulombsLaw2.java
+++ src/com/eblackwelder/physx/CoulombsLaw2.java
@@ -3,6 +3,7 @@
  */
 package com.eblackwelder.physx;
 
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 import com.eblackwelder.graphx.renderable.Renderable;
@@ -31,39 +32,61 @@ public class CoulombsLaw2 extends CoulombsLaw {
 		this.renderableCreator = renderableCreator;
 	}
 
+	//TODO: bugs...
+	//Overlay items are left lying around (a limitation of the rendering strategy)
+	//Doesn't handle collisions between more than 2 particles well.
 	@Override
 	public void apply(Mass a1, Mass a2, Rectangle2D b1, Rectangle2D b2) {
 		if (a1.getPosition().distance(a2.getPosition()) < minCombiningDistance) {
 			Charge c1 = (Charge) a1;
 			Charge c2 = (Charge) a2;
 			if (c1.getCharge() > 0 != c2.getCharge() > 0) { //combine oppositely charged particles :)
-				double charge = c1.getCharge() + c2.getCharge();
-				double m1 = c1.getMass();
-				double m2 = c2.getMass();
-				double mass = m1 + m2;
-
-				Vector2D p1 = c1.getPosition();
-				Vector2D p2 = c2.getPosition();
-				Vector2D position = Vector2D.midPoint(p1, p2);
-				
-				Vector2D v1 = c1.getVelocity();
-				Vector2D v2 = c2.getVelocity();
-				//conservation of momentum:
-				//m1v1 + m2v2 = (m1+m2)v'
-				//v' = (m1*v1 + m2*v2) / (m1+m2)
-				Vector2D velocity = Vector2D.scale(v1, m1).add(Vector2D.scale(v2, m2)).scale(1 / (m1+m2));
-				
-				Renderable renderable = renderableCreator.create(mass, charge);
-				ChargedParticle newParticle = new ChargedParticle(renderable, mass, charge, position, velocity);
+				Charge newParticle = combineParticles(c1, c2);
 				
 				World world = World.getSingleton();
-				world.remove(a1); //TODO : this doesn't play nicely with Overlay items.
-				world.remove(a2);
+				world.remove(c1); //TODO : this doesn't play nicely with Overlay items.
+				world.remove(c2);
 				world.add(newParticle);
 			}
 		} else {
 			super.apply(a1, a2, b1, b2);
 		}
+	}
+
+	private Charge combineParticles(Charge ...charges) {
+		double newCharge = 0;
+		double totalMass = 0;
+		
+		double minX = Double.POSITIVE_INFINITY, maxX = Double.NEGATIVE_INFINITY;
+		double minY = Double.POSITIVE_INFINITY, maxY = Double.NEGATIVE_INFINITY;
+
+		Vector2D velocity = new Vector2D(0, 0);
+		for (Charge c : charges) {
+			newCharge += c.getCharge();
+			totalMass += c.getMass();
+
+			double x = c.getPosition().x;
+			double y = c.getPosition().y;
+			minX = Math.min(minX,  x);
+			maxX = Math.max(maxX,  x);
+			minY = Math.min(minY,  y);
+			maxY = Math.max(maxY,  y);
+			
+			velocity = velocity.add(Vector2D.scale(c.getVelocity(), c.getMass()));
+		}
+		//positioned at the exact midpoint (should it actually be the center of mass?)
+		double x = minX + (maxX - minX) / 2.0;
+		double y = minY + (maxY - minY) / 2.0;
+		Point2D position = new Point2D.Double(x, y);
+
+		//conservation of momentum:
+		//m1v1 + m2v2 = (m1+m2)v'
+		//v' = (m1*v1 + m2*v2) / (m1+m2)
+		velocity = velocity.scale(1.0 / totalMass);
+		
+		Renderable renderable = renderableCreator.create(totalMass, newCharge);
+		ChargedParticle newParticle = new ChargedParticle(renderable, totalMass, newCharge, position, velocity);
+		return newParticle;
 	}
 
 }
